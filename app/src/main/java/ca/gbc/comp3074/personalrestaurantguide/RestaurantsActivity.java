a2c2763e6cde6a5acac6a9bc3f1e10a8cdcb0485
beatriz2889 <beatriz.morales@georgebrown.ca>
2020-11-19 17:59:52 -0500

    Add restaurant screen working (writes to file), edit restaurant blank screen, added more styles.

--- app/src/main/java/ca/gbc/comp3074/personalrestaurantguide/RestaurantsActivity.java
+++ app/src/main/java/ca/gbc/comp3074/personalrestaurantguide/RestaurantsActivity.java
@@ -4,16 +4,91 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class RestaurantsActivity extends AppCompatActivity {
+
+    private TextView restaurantsTitle;
+    private TextView getRestaurantName;
+    private TextView getTags;
+    private ListView restaurantsList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        restaurantsTitle=(TextView)findViewById(R.id.restaurantsTitle);
+        restaurantsList=(ListView)findViewById(R.id.restaurantsList);
+
+        //List<String[]>entries=new ArrayList<String[]>();
+      /* String[]entriesall = new String[];
+
+        try(BufferedReader br=new BufferedReader(new FileReader("Restaurant-Data.txt"))){
+            String line;
+            while((line=br.readLine())!=null) {
+                String[] entries = line.split("\n");
+                entriesall.add(Arrays.asList(entries));
+
+
+            }
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+*/
+
+
+           /* try{
+                FileInputStream fileInputStream= openFileInput("Restaurant-Data.txt");
+                String line;
+                int read=-1;
+                StringBuffer buffer=new StringBuffer();
+                while((read=fileInputStream.read())!=-1){
+                    buffer.append((char)read);
+
+                }
+                Log.d("Restaurant-Data",buffer.toString());
+                String name=buffer.substring(0,buffer.indexOf(","));
+                String address=buffer.substring(buffer.indexOf(",")+1);
+                String phone=buffer.substring(buffer.indexOf(",")+2);
+                String description=buffer.substring(buffer.indexOf(",")+3);
+                String rating=buffer.substring(buffer.indexOf(",")+4);
+                String tags=buffer.substring(buffer.indexOf(",")+5);
+                entries= new String[]{name, address,}
+
+
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+            */
+
+        /*
+        ArrayAdapter adapter=new ArrayAdapter<String>(this,R.layout.activity_main,entries);
+            ListView listView=(ListView)findViewById(R.id.restaurantsList);
+            listView.setAdapter(adapter);
+
+            */
+
+
+
+
 
     }
 
@@ -48,4 +123,6 @@ public class RestaurantsActivity extends AppCompatActivity {
         }
         return true;
     }
+
+
 }
